2a5a6d9f0c75be064451f0e8dcd01b41f4c191cf
viluong <viluong1610@gmail.com>
2025-01-24 16:45:02 +0700

    remove logger

--- server/app/services/redis.py
+++ server/app/services/redis.py
@@ -1,17 +1,15 @@
 import logging
 
 from redis.asyncio import Redis, ConnectionPool
+
 from app.config import settings
 
 logger = logging.getLogger(__name__)
 
 redis_pool = ConnectionPool.from_url(
-    f"redis://{settings.redis_host}:{settings.redis_port}",
-    db=0,
-    max_connections=10
+    f"redis://{settings.redis_host}:{settings.redis_port}", db=0, max_connections=10
 )
 
 
 async def get_redis():
-    logger.debug("bbbbbbbbbbbbbbbbbbbbbb")
     return Redis(connection_pool=redis_pool, decode_responses=True)
